ac15d72a20af48f00b0d9b5fca819422f93470c7
truetamplin <truetamplin@gmail.com>
2018-02-18 05:11:42 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,8 @@
 # Write your #display_rainbow method here
 
 def display_rainbow (['red','orange','yellow','green','blue','indigo','violet'])
-  puts "R: display_rainbow[0]"
+  colors = ['red','orange','yellow','green','blue','indigo','violet']
+  puts "R: colors[0]"
 end
+
+display_rainbow
